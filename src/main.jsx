e905c1853ef49b654dbbc6c298b6852467d87661
Antonios Kokiantonis <kokiantonisltd@gmail.com>
2024-10-23 15:10:53 +0300

    Update main.jsx
    change code with reactDom and routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter, Routes, Route} from "react"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById("root")
+                    <StrictMode>
+                      <BrowserRouter basename={import.meta.env.BASE_URL)
+                        <Routes>
+                          <Route path= "*" element={<App />} />
+                        </Routes>
+                        </BrowserRouter>
+                          </StrictMode>
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
 )
